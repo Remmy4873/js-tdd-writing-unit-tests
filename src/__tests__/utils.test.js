8b8717724fc12b41b327d1c8233a4871decbeab9
Remmy4873 <Remmykiplangat4873@gmail.com>
2024-08-09 13:31:33 +0300

    initial commit

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,22 @@
 // Your tests here
+function pointsForWord(word) {
+    let points = 0;
+    for (const char of word) {
+      points += /[aeiou]/i.test(char) ? 1 : 2;
+    }
+    return points;
+  }
+  
+  describe("pointsForWord", () => {
+    it("returns the correct number of points for a word", () => {
+      const word = "test";
+      const points = pointsForWord(word);
+      expect(points).toBe(7);
+    });
+  
+    it("handles uppercase and lowercase input", () => {
+      const word = "tEsT";
+      const points = pointsForWord(word);
+      expect(points).toBe(7);
+    });
+  });
